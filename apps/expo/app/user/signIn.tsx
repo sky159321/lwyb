c030a8fb8b36efaa62b6647292a74cf435a0bc0f
SomeDooDoo <sky9173546@gmail.com>
2023-07-25 11:41:47 -0700

    yellow theme

--- apps/expo/app/user/signIn.tsx
+++ apps/expo/app/user/signIn.tsx
@@ -1,15 +1,16 @@
+import { Theme } from 'tamagui'
 import { SignIn } from 'app/features/auth/signIn'
 import { Stack } from 'expo-router'
 
 export default function Screen() {
   return (
-    <>
+    <Theme name="yellow">
       <Stack.Screen
         options={{
           title: 'Sign In',
         }}
       />
       <SignIn />
-    </>
+    </Theme>
   )
 }
